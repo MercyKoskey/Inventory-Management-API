06e4c63e4ed867c0e72ae8cd4becd6a0d48520cc
Mercy Koskey <koskey.mercy@gmail.com>
2025-08-28 23:38:33 +0300

    created authentication app

---
+++ authentication/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import CustomUser
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add extra claims if needed
+        token['email'] = user.email
+        token['username'] = user.username
+        return token
+
+    def validate(self, attrs):
+        # Accepts "email" instead of "username"
+        attrs['username'] = attrs.get('email')
+        return super().validate(attrs)
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'password')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        return user
